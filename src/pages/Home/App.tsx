7d3ccd9acc8d3616b4dd78caf24bebed2fa216d3
Anfelesan <anfelesan@gmail.com>
2025-04-28 04:43:29 -0500

    [Addition] Add temporal home page

---
+++ src/pages/Home/App.tsx
@@ -0,0 +1,13 @@
+import css from './App.module.css'
+
+function App() {
+  return (
+    <>
+      <main className={css.main}>
+        <h1 className={css.title}>Anfelesan</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
